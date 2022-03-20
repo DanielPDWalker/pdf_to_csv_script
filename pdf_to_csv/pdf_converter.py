c2b2baa5dd21d83857cb8a690509b7453f1f2df1
Daniel Walker <danielpdwalker@gmail.com>
2022-03-20 15:19:21 +0000

    Changed output path to be actual path type.

--- pdf_to_csv/pdf_converter.py
+++ pdf_to_csv/pdf_converter.py
@@ -1,13 +1,19 @@
 import tabula as tb
 import os
 
+from pathlib import Path
+
 
 def convert_pdf(pdf_file_path):
 
-    os.path.abspath(pdf_file_path)
+    pdf_file_path = os.path.abspath(pdf_file_path)
 
     head, tail = os.path.split(pdf_file_path)
 
     tail = tail.split(".")[0]
 
-    tb.convert_into(pdf_file_path, f"{head}/{tail}.csv", output_format="csv", pages='all')
+    head = Path(head)
+
+    output_path = os.path.join(head, tail + ".csv")
+
+    tb.convert_into(pdf_file_path, output_path, output_format="csv", pages='all')
